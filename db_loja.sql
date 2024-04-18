49cb1cc7cf341b44111d1a04d02f053c4f6b47c7
marcossv99 <marcoss.gwp@gmail.com>
2024-04-17 22:01:38 -0300

    sql update

--- db_loja.sql
+++ db_loja.sql
@@ -1,15 +1,27 @@
-create table clientes(
-	id serial primary key,
-	nome text not null,
-	email text not null unique,
-	senha text not null,
-	tipo text default 'cliente'
-)	
+create table clientes
+(
+    id    serial primary key,
+    nome  text not null,
+    email text not null unique,
+    senha text not null,
+    tipo  text default 'cliente'
+)
 
-create table lojista(
-	id serial primary key,
-	nome text not null,
-	email text not null unique,
-	senha text not null,
-	tipo text default 'lojista'
-)	
+create table lojista
+(
+    id    serial primary key,
+    nome  text not null,
+    email text not null unique,
+    senha text not null,
+    tipo  text default 'lojista'
+)
+
+create table produtos
+(
+    id        serial primary key,
+    nome      text not null,
+    descricao text not null,
+    preco     int  not null,
+    estoque   int  not null,
+
+)
